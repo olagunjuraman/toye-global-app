efd2e572485d61f38178fb323433db3c250fa394
raman <raman@figo.africa>
2024-07-15 14:25:43 +0100

    added-delay

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello from Simple Node App!');
 });
 
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is runner on port ${PORT}`);
 });
